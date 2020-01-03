a36ba6fd970f6adfb75b20f377f983f3a64627cc
Ray Huang <huang.jingze@columbia.edu>
2020-01-02 20:07:19 -0800

    [New] Feature home

---
+++ feature_home/src/main/java/com/ray/utopia/feature/home/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.ray.utopia.feature.home;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+import com.ray.utopia.core.BaseActivity;
+
+public class HomeActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
